48869c10cf35f46d835f877db9ab7a8e0362fa85
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 12:47:00 +0200

    feat(y2015;2d): setup correct data structure

--- y2015/_shared/readFile.js
+++ y2015/_shared/readFile.js
@@ -4,6 +4,6 @@ export async function readText(input) {
   return await readFile(input, 'utf8'); 
 }
 
-export function manage_multiple_lines(input) {    
+export function manageMultipleLines(input) {    
     return input.replace(/\r\n?/g, '\n').trimEnd().split('\n');
 }
