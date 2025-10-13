0b78c0a53eff99ecb2feda766719d42717aecc76
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 12:47:00 +0200

    feat(y2015;2d): setup correct data structure

--- y2015/d2/algorithm.js
+++ y2015/d2/algorithm.js
@@ -1,10 +1,21 @@
-import { manage_multiple_lines } from "../_shared/readFile.js"
+import { manageMultipleLines } from "../_shared/readFile.js"
+
+function parseSize(str) {
+    const size = str.split('x');
+
+    return {
+        length: size[0],
+        width: size[1],
+        height: size[2]
+    }
+}
 
 export function total_square_feet_wrapping_paper(input) {
-    const sizes = manage_multiple_lines(input)
+    const sizes = manageMultipleLines(input)
     let total = 0;
     
-    for (const size of sizes) {
+    for (const sizeString of sizes) {
+        const size = parseSize(sizeString);
         console.log(size)
     }
 
