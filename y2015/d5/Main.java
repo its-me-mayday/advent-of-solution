fba5101a35135960243da59f7e883baa74a33245
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 22:46:27 +0200

    feat(y2015;d5): setup checkTwiceLetterInARow

--- y2015/d5/Main.java
+++ y2015/d5/Main.java
@@ -1,6 +1,5 @@
 package d5;
 
-import java.util.HashSet;
 import java.util.Set;
 
 class Main{
@@ -17,11 +16,11 @@ class Main{
         final String inputTest4 = "haegwjzuvuyypxyu";
         final String inputTest5 = "dvszwmarrgswjxmb";
         
-        System.out.println(isNiceString(inputTest1));
-        System.out.println(isNiceString(inputTest2));
-        System.out.println(isNiceString(inputTest3));
-        System.out.println(isNiceString(inputTest4));
-        System.out.println(isNiceString(inputTest5));
+        System.out.println("is Nice?" + isNiceString(inputTest1));
+        System.out.println("is Nice?" + isNiceString(inputTest2));
+        System.out.println("is Nice?" + isNiceString(inputTest3));
+        System.out.println("is Nice?" + isNiceString(inputTest4));
+        System.out.println("is Nice?" + isNiceString(inputTest5));
     }
     
     private static final boolean isNiceString(final String str) {
@@ -40,16 +39,22 @@ class Main{
         int countVowels = 0;
         
         for(int i=0; i<strLenght;i++) {
-            char ch = str.charAt(i);
-            if (VOWELS.indexOf(ch) >= 0 && ++countVowels >= MIN_VOWELS) {
-            return true; 
-        }
+            final char ch = str.charAt(i);
+            if (VOWELS.indexOf(ch) >= 0 && ++countVowels >= MIN_VOWELS) return true; 
         }
 
-        return countVowels >= MIN_VOWELS;
+        return (countVowels >= MIN_VOWELS);
     }
      
     private static final boolean checkTwiceLetterInARow(final String str) {
-        return true;
+        final int strLenght = str.length();
+        
+        for(int i=1; i<strLenght;i++) {
+            final char chPreavious = str.charAt(i-1);
+            final char chCurrent   = str.charAt(i);
+            if(chPreavious == chCurrent) return true;
+                
+        }
+        return false;
     }
 }
