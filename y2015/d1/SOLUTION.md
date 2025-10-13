743c8f6907328abae6f344a8e6aba0ea6d3a4b24
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 11:55:53 +0200

    feat(y2015;1d): conclude part 2

--- y2015/d1/SOLUTION.md
+++ y2015/d1/SOLUTION.md
@@ -1,2 +1,4 @@
 ## To what floor do the instructions take Santa?
-<ANSWER>
+138
+## What is the position of the character that causes Santa to first enter the basement?
+1771
--- y2015/d1/main.js
+++ y2015/d1/main.js
@@ -1,33 +1,64 @@
 import { readText } from "../_shared/readFile.js"
 
-const instructions = { '(': 1, ')': -1 };
+const INSTRUCTIONS = { '(': 1, ')': -1 };
+const SOURCE_FLOOR = 0;
+const START_POSITION = 1;
 
 function step(c){
-  return (instructions[c] ?? 0);
+  return (INSTRUCTIONS[c] ?? 0);
 }
 
-function go_to_floor(path, floor) {
-    let pathLength = path.length
+function hasEncounterBasement(basement, floor) {
+  return (!basement && isBasement(floor));
+}
+
+function isBasement(floor) {
+  return (floor == -1) ?? false;
+}
+
+function go_to_floor(path) {
+    let basementEncountered = false;
+    let first_basement_position = -1;
+    let floor = SOURCE_FLOOR;
+    let pathLength = path.length;
+    let position = START_POSITION;
 
     for (let i=0; i<pathLength; i++) {
-        let instruction = path[i]
-        floor += step(instruction)
+        let instruction = path[i];
+        floor += step(instruction);
+        if (hasEncounterBasement(basementEncountered, floor)) {
+          basementEncountered = true;
+          first_basement_position = position;
+        }
+        else position++;
     }
-
-    return floor
+    
+    return {floor: floor, first_basement_position: first_basement_position}
 }
 
-const SOURCE_FLOOR = 0;
+let pathi1 = await readText("input_p1.txt");
+let pathi2 = await readText("input_p2.txt");
 
-console.log("===== TEST INPUTS ====")
-console.log(`Santa, is at floor: ${go_to_floor("(())", SOURCE_FLOOR)}`)
-console.log(`Santa, is at floor: ${go_to_floor("(()(()(", SOURCE_FLOOR)}`)
-console.log(`Santa, is at floor: ${go_to_floor("(())", SOURCE_FLOOR)}`)
-console.log(`Santa, is at floor: ${go_to_floor("))(((((", SOURCE_FLOOR)}`)
-console.log(`Santa, is at floor: ${go_to_floor("))(", SOURCE_FLOOR)}`)
-console.log(`Santa, is at floor: ${go_to_floor(")())())", SOURCE_FLOOR)}`)
+console.log("===== PART ONE - TEST INPUTS ====");
+console.log(`Santa, is at floor: ${go_to_floor("(())").floor}`);
+console.log(`Santa, is at floor: ${go_to_floor("(()(()(").floor}`);
+console.log(`Santa, is at floor: ${go_to_floor("(())").floor}`);
+console.log(`Santa, is at floor: ${go_to_floor("))(((((").floor}`);
+console.log(`Santa, is at floor: ${go_to_floor("))(").floor}`);
+console.log(`Santa, is at floor: ${go_to_floor(")())())").floor}`);
+console.log("===== PART ONE - TEST TASK INPUT 1 (input_p1.txt) ====");
+console.log(`Santa, is at floor: ${go_to_floor(pathi1).floor}`);
 
-
-console.log("===== TEST TASK INPUT 1 (input_p1.txt) ====")
-let pathi1 = await readText("input_p1.txt")
-console.log(`Santa, is at floor: ${go_to_floor(pathi1, SOURCE_FLOOR)}`)
+console.log("===== PART TWO - TEST INPUTS ====");
+console.log("== PART TWO - TEST 1 ==");
+console.log(`Santa, is at floor: ${go_to_floor(")").floor}`);
+console.log(`Santa, the first time that you enter in basement (floor=-1) is at position: ${go_to_floor(")").first_basement_position}`);
+console.log("== PART TWO - TEST 2 ==");
+console.log(`Santa, is at floor: ${go_to_floor("()())").floor}`);
+console.log(`Santa, the first time that you enter in basement (floor=-1) is at position: ${go_to_floor("()())").first_basement_position}`);
+console.log("== PART TWO - TEST 3 ==");
+console.log(`Santa, is at floor: ${go_to_floor("(())").floor}`);
+console.log(`Santa, the first time that you enter in basement (floor=-1) is at position: ${go_to_floor("(())").first_basement_position}`);
+console.log("===== PART TWO - TEST TASK INPUT 1 (input_p2.txt) ====");
+console.log(`Santa, is at floor: ${go_to_floor(pathi2).floor}`);
+console.log(`Santa, the first time that you enter in basement (floor=-1) is at position: ${go_to_floor(pathi2).first_basement_position}`);
