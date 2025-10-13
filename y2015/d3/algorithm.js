013a6655fbcc10a272fe163c23a1cb1a98f89d89
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 14:04:09 +0200

    docs(y2015;d3): setup day3

---
+++ y2015/d3/algorithm.js
@@ -0,0 +1,13 @@
+
+const MOVEMENTS = { 
+    '^': (0,1), 
+    'v': (0,-1),
+    '>': (1,0),
+    '<': (-1,0)
+};
+
+export function getNumberHappyHouses(path) {
+    let happyHouses = 1;
+
+    return happyHouses;
+}
