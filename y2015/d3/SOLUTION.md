39995424fabc4266aa5e424d72265c740bf94a9c
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 15:07:09 +0200

    feat(y2015;d3): conclude part2

--- y2015/d3/SOLUTION.md
+++ y2015/d3/SOLUTION.md
@@ -1,1 +1,4 @@
 ## How many houses receive at least one present?
+2572
+## This year, how many houses receive at least one present?
+2631
--- y2015/d3/main.js
+++ y2015/d3/main.js
@@ -1,5 +1,5 @@
 import { readText } from "../_shared/readFile.js"
-import { getNumberHappyHouses } from "./algorithm.js"
+import { getNumberHappyHouses, getNumberHappyHousesWithRoboSanta } from "./algorithm.js"
 
 let sizeInput1 = await readText("inputs/part1.txt");
 let sizeInput2 = await readText("inputs/part2.txt");
@@ -14,3 +14,14 @@ console.log(`Santa, houses that received at least one present are: ${getNumberHa
 console.log(`Santa, houses that received at least one present are: ${getNumberHappyHouses(testInput3)}`);
 console.log("===== PART ONE - TEST TASK INPUT 1 (inputs/part1.txt) ====");
 console.log(`Santa, houses that received at least one present are: ${getNumberHappyHouses(sizeInput1)}`);
+
+const testInput4 = "^v"; 
+const testInput5 = "^>v<";
+const testInput6 = "^v^v^v^v^v";
+
+console.log("===== PART TWO - TEST INPUTS ====");
+console.log(`Santa and RoboSanta, houses that received at least one present are: ${getNumberHappyHousesWithRoboSanta(testInput4)}`);
+console.log(`Santa and RoboSanta, houses that received at least one present are: ${getNumberHappyHousesWithRoboSanta(testInput5)}`);
+console.log(`Santa and RoboSanta, houses that received at least one present are: ${getNumberHappyHousesWithRoboSanta(testInput6)}`);
+console.log("===== PART TWO - TEST TASK INPUT 2 (inputs/part2.txt) ====");
+console.log(`Santa and RoboSanta, houses that received at least one present are: ${getNumberHappyHousesWithRoboSanta(sizeInput2)}`);
