bc26647b368d632a0121d9dc90f1e50463ad9e24
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 13:42:05 +0200

    docs(y2015;d2): conclude part 2

--- y2015/d2/algorithm.js
+++ y2015/d2/algorithm.js
@@ -1,4 +1,3 @@
-import { get } from "http";
 import { manageMultipleLines } from "../_shared/readFile.js"
 
 function parseSize(str) {
@@ -12,7 +11,7 @@ function parseSize(str) {
 }
 
 function getAreaData(size) {
-return    {
+    return {
             lxw: size.length * size.width,
             wxh: size.width * size.height,
             hxl: size.height * size.length
@@ -23,7 +22,7 @@ function getAreaSmallestSide(area) {
     return Math.min(area.hxl, area.lxw, area.wxh);
 }
 
-export function total_square_feet_wrapping_paper(input) {
+export function totalSquareFeetWrappingPaper(input) {
     const sizes = manageMultipleLines(input)
     let total = 0;
     
@@ -31,6 +30,34 @@ export function total_square_feet_wrapping_paper(input) {
         const size = parseSize(sizeString);
         const areaData = getAreaData(size);
         const totalArea = (2*areaData.lxw + 2*areaData.wxh + 2*areaData.hxl) + getAreaSmallestSide(areaData);
+
+        total+=totalArea
+    }
+
+    return total;
+}
+
+function getPerimeterData(size) {
+    return {
+            p1: 2*size.length + 2*size.width,
+            p2: 2*size.width + 2*size.height,
+            p3: 2*size.height + 2*size.length
+        }
+}
+
+function getSmallestPerimeter(perimeter) {
+    return Math.min(perimeter.p1, perimeter.p2, perimeter.p3)
+}
+
+export function totalFeetOfRibbon(input) {
+    const sizes = manageMultipleLines(input)
+    let total = 0;
+    
+    for (const sizeString of sizes) {
+        const size = parseSize(sizeString);
+        const perimeterData = getPerimeterData(size);
+        const volume = size.length * size.width * size.height;
+        const totalArea = volume + getSmallestPerimeter(perimeterData);
 
         total+=totalArea
     }
--- y2015/d2/main.js
+++ y2015/d2/main.js
@@ -1,5 +1,5 @@
 import { readText } from "../_shared/readFile.js"
-import { total_square_feet_wrapping_paper } from "./algorithm.js"
+import { totalSquareFeetWrappingPaper, totalFeetOfRibbon } from "./algorithm.js"
 
 let sizeInput1 = await readText("inputs/part1.txt");
 let sizeInput2 = await readText("inputs/part2.txt");
@@ -8,12 +8,13 @@ const testInput1 = "2x3x4";
 const testInput2 = "1x1x10";
 
 console.log("===== PART ONE - TEST INPUTS ====");
-console.log(`Elves, the total square feet of wrapping paper is: ${total_square_feet_wrapping_paper(testInput1)}`);
-console.log(`Elves, the total square feet of wrapping paper is: ${total_square_feet_wrapping_paper(testInput2)}`);
+console.log(`Elves, the total square feet of wrapping paper is: ${totalSquareFeetWrappingPaper(testInput1)}`);
+console.log(`Elves, the total square feet of wrapping paper is: ${totalSquareFeetWrappingPaper(testInput2)}`);
 console.log("===== PART ONE - TEST TASK INPUT 1 (inputs/part1.txt) ====");
-console.log(`Elves, the total square feet of wrapping paper is: ${total_square_feet_wrapping_paper(sizeInput1)}`);
+console.log(`Elves, the total square feet of wrapping paper is: ${totalSquareFeetWrappingPaper(sizeInput1)}`);
 
 console.log("===== PART TWO - TEST INPUTS ====");
-console.log("===== PART TWO - <TODO> ====");
-console.log("===== PART ONE - TEST TASK INPUT 2 (inputs/part2.txt) ====");
-console.log("===== <TODO> ====");
+console.log(`Elves, the total feet of ribbon is: ${totalFeetOfRibbon(testInput1)}`);
+console.log(`Elves, the total feet of ribbon is: ${totalFeetOfRibbon(testInput2)}`);
+console.log("===== PART TWO - TEST TASK INPUT 2 (inputs/part2.txt) ====");
+console.log(`Elves, the total square feet of wrapping paper is: ${totalFeetOfRibbon(sizeInput2)}`);
