386573a0eb8d54c1ec015c456d7df55a5447dc05
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 22:50:17 +0200

    feat(y2015;d5): init countNiceStrings function

--- y2015/d5/Main.java
+++ y2015/d5/Main.java
@@ -16,11 +16,19 @@ class Main{
         final String inputTest4 = "haegwjzuvuyypxyu";
         final String inputTest5 = "dvszwmarrgswjxmb";
         
-        System.out.println("is Nice?" + isNiceString(inputTest1));
-        System.out.println("is Nice?" + isNiceString(inputTest2));
-        System.out.println("is Nice?" + isNiceString(inputTest3));
-        System.out.println("is Nice?" + isNiceString(inputTest4));
-        System.out.println("is Nice?" + isNiceString(inputTest5));
+        System.out.println("is Nice? " + isNiceString(inputTest1));
+        System.out.println("is Nice? " + isNiceString(inputTest2));
+        System.out.println("is Nice? " + isNiceString(inputTest3));
+        System.out.println("is Nice? " + isNiceString(inputTest4));
+        System.out.println("is Nice? " + isNiceString(inputTest5));
+        
+        final String inputPartOnePath = "./inputs/part1.txt";
+        
+        System.out.println("How many nice strings? " + countNiceStrings(inputPartOnePath));
+    }
+    
+    private static final int countNiceStrings(final String str){
+       return 0;
     }
     
     private static final boolean isNiceString(final String str) {
@@ -52,8 +60,7 @@ class Main{
         for(int i=1; i<strLenght;i++) {
             final char chPreavious = str.charAt(i-1);
             final char chCurrent   = str.charAt(i);
-            if(chPreavious == chCurrent) return true;
-                
+            if(chPreavious == chCurrent) return true; 
         }
         return false;
     }
