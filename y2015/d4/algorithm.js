59e53fa31fba3863b1d47dc9e6db9b93f3d079c2
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-13 16:57:24 +0200

    feat(y2015;d4): conclude part 1

--- y2015/d4/algorithm.js
+++ y2015/d4/algorithm.js
@@ -1,5 +1,34 @@
-export function getHashAdventCoin(secretFile) {
-    let hashAdventCoin = ""
+import crypto from 'node:crypto';
 
-    return hashAdventCoin;
+const re = /^0{5,}/;
+
+function md5(text) {
+  return crypto.createHash('md5').update(text).digest('hex');
+}
+
+function isAnAccettableHash(hash) {
+  return typeof hash === 'string' && re.test(hash);
+}
+
+export function getHashAdventCoinPayload(secret) {
+    let hashAdventCoin = md5(secret);
+    let payload = 0;
+    
+    console.log(isAnAccettableHash(hashAdventCoin))
+
+    if(!isAnAccettableHash(hashAdventCoin)){
+        console.log("Searching payload...")
+       do {
+           let secretManipulated = `${secret}${payload}`
+           hashAdventCoin = md5(secretManipulated)
+           payload += 1
+
+       }while(!isAnAccettableHash(hashAdventCoin));
+    }
+    else
+        return payload
+
+    console.log(`The hash that starts with at least 5 zeros is: ${hashAdventCoin}`)
+    console.log(`The entire secret is: ${secret}${payload-1}`)
+    return payload-1;
 }
--- y2015/d4/main.js
+++ y2015/d4/main.js
@@ -1,12 +1,14 @@
-import { getHashAdventCoin } from "./algorithm.js"
+import { getHashAdventCoinPayload } from "./algorithm.js"
 
 const testInput1 = "abcdef"; 
 const testInput2 = "pqrstuv";
+const testInput3 = "abcdef609043"; 
 
 const inputPartOne = "ckczppom";
 
 console.log("===== PART ONE - TEST INPUTS ====");
-console.log(`Santa, the hash of adventCoin is: ${getHashAdventCoin(testInput1)}`);
-console.log(`Santa, the hash of adventCoin is: ${getHashAdventCoin(testInput2)}`);
+console.log(`Santa, the payload of adventCoin hash is: ${getHashAdventCoinPayload(testInput1)}`);
+console.log(`Santa, the hash of adventCoin is: ${getHashAdventCoinPayload(testInput2)}`);
+console.log(`Santa, the payload of adventCoin hash is: ${getHashAdventCoinPayload(testInput3)}`);
 console.log("===== PART ONE - TEST TASK INPUT 1 ====");
-console.log(`Santa, the hash of adventCoin is: ${getHashAdventCoin(inputPartOne)}`);
+console.log(`Santa, the hash of adventCoin is: ${getHashAdventCoinPayload(inputPartOne)}`);
