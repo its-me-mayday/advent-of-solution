d7f683d6413189ef8a9e4e186dfee7b4ab6786c9
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-14 09:02:47 +0200

    style(y2015;d5): move all methods in NiceString class

---
+++ y2015/d5/NiceString.java
@@ -0,0 +1,49 @@
+package d5;
+
+import java.util.Set;
+
+public class NiceString { 
+    private final int MIN_VOWELS = 3;
+    private final String VOWELS = "aeiouAEIOU";
+    private static final Set<String> proibitedStrings = Set.of("ab", "cd", "pq", "xy");
+
+    private final String str;
+
+    public NiceString(final String str) {
+        this.str = str;
+    } 
+    
+    public boolean isNiceString() {
+        return checkProibitedStrings(this.str) &&
+            checkVowels(this.str) &&
+            checkTwiceLetterInARow(this.str);
+    }
+    
+    private boolean checkProibitedStrings(final String str) {
+        if(str == null) return false;
+        return proibitedStrings.stream().noneMatch(str::contains);
+    }
+    
+    private boolean checkVowels(final String str) {
+        final int strLenght = str.length();
+        int countVowels = 0;
+        
+        for(int i=0; i<strLenght;i++) {
+            final char ch = str.charAt(i);
+            if (VOWELS.indexOf(ch) >= 0 && ++countVowels >= MIN_VOWELS) return true; 
+        }
+
+        return (countVowels >= MIN_VOWELS);
+    }
+     
+    private boolean checkTwiceLetterInARow(final String str) {
+        final int strLenght = str.length();
+        
+        for(int i=1; i<strLenght;i++) {
+            final char chPreavious = str.charAt(i-1);
+            final char chCurrent   = str.charAt(i);
+            if(chPreavious == chCurrent) return true; 
+        }
+        return false;
+    }
+}
