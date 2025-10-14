75fc53e92ff4431c09a883f0c6373c96d0173cca
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-14 09:02:47 +0200

    style(y2015;d5): move all methods in NiceString class

--- y2015/d5/Main.java
+++ y2015/d5/Main.java
@@ -1,19 +1,10 @@
 package d5;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class Main{
-    private static final int MIN_VOWELS = 3;
-    private static final String VOWELS = "aeiouAEIOU";
-    private static final Set<String> proibitedStrings = Set.of("ab", "cd", "pq", "xy");
+public class Main {
     
     public static void main(String[]args) throws IOException{
         System.out.println("==== Doesn't He Have Intern-Elves For This? =====");
@@ -26,11 +17,20 @@ public class Main{
         
         final String inputPartOnePath = "./inputs/part1.txt";
         
-        System.out.println("is Nice? " + isNiceString(inputTest1));
-        System.out.println("is Nice? " + isNiceString(inputTest2));
-        System.out.println("is Nice? " + isNiceString(inputTest3));
-        System.out.println("is Nice? " + isNiceString(inputTest4));
-        System.out.println("is Nice? " + isNiceString(inputTest5));
+        NiceString niceString = new NiceString(inputTest1); 
+        System.out.println("is Nice? " + niceString.isNiceString());
+        
+        niceString = new NiceString(inputTest2); 
+        System.out.println("is Nice? " + niceString.isNiceString());
+        
+        niceString = new NiceString(inputTest3); 
+        System.out.println("is Nice? " + niceString.isNiceString());
+        
+        niceString = new NiceString(inputTest4); 
+        System.out.println("is Nice? " + niceString.isNiceString());
+        
+        niceString = new NiceString(inputTest5); 
+        System.out.println("is Nice? " + niceString.isNiceString());
         
         final List<String> strings = FileUtils.readLines(Paths.get(inputPartOnePath));
         System.out.println("How many nice strings? " + countNiceStrings(strings));
@@ -39,45 +39,13 @@ public class Main{
     private static final int countNiceStrings(final List<String> strings){
         final int listSize = strings.size();
         int countNiceStrings = 0;
+        NiceString niceString = null;
 
         for(int i=0; i< listSize; i++) {
             final String str = strings.get(i);
-            if(isNiceString(str)) countNiceStrings++; 
+            niceString = new NiceString(str);
+            if(niceString.isNiceString()) countNiceStrings++; 
         }
         return countNiceStrings;
-    }
-    
-    private static final boolean isNiceString(final String str) {
-        return checkProibitedStrings(str) &&
-            checkVowels(str) &&
-            checkTwiceLetterInARow(str);
-    }
-    
-    private static final boolean checkProibitedStrings(final String str) {
-        if(str == null) return false;
-        return proibitedStrings.stream().noneMatch(str::contains);
-    }
-    
-    private static final boolean checkVowels(final String str) {
-        final int strLenght = str.length();
-        int countVowels = 0;
-        
-        for(int i=0; i<strLenght;i++) {
-            final char ch = str.charAt(i);
-            if (VOWELS.indexOf(ch) >= 0 && ++countVowels >= MIN_VOWELS) return true; 
-        }
-
-        return (countVowels >= MIN_VOWELS);
-    }
-     
-    private static final boolean checkTwiceLetterInARow(final String str) {
-        final int strLenght = str.length();
-        
-        for(int i=1; i<strLenght;i++) {
-            final char chPreavious = str.charAt(i-1);
-            final char chCurrent   = str.charAt(i);
-            if(chPreavious == chCurrent) return true; 
-        }
-        return false;
     }
 }
