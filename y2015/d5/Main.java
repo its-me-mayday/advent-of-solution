9d2d07ad10fe84d1e96bb94dfcb7eab92faa6336
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-14 09:16:53 +0200

    style(y2015;d5): improve iteration

--- y2015/d5/Main.java
+++ y2015/d5/Main.java
@@ -6,31 +6,27 @@ import java.util.List;
 
 public class Main {
     
+    final static String[] inputTests = {
+        "ugknbfddgicrmopn",
+        "aaa",
+        "jchzalrnumimnmhp",
+        "haegwjzuvuyypxyu",
+        "dvszwmarrgswjxmb"
+    }; 
+    
+    final static String inputPartOnePath = "./inputs/part1.txt";
+    
     public static void main(String[]args) throws IOException{
         System.out.println("==== Doesn't He Have Intern-Elves For This? =====");
-        
-        final String inputTest1 = "ugknbfddgicrmopn";
-        final String inputTest2 = "aaa";
-        final String inputTest3 = "jchzalrnumimnmhp";
-        final String inputTest4 = "haegwjzuvuyypxyu";
-        final String inputTest5 = "dvszwmarrgswjxmb";
-        
-        final String inputPartOnePath = "./inputs/part1.txt";
-        
-        NiceString niceString = new NiceString(inputTest1); 
-        System.out.println("is Nice? " + niceString.isNiceString());
-        
-        niceString = new NiceString(inputTest2); 
-        System.out.println("is Nice? " + niceString.isNiceString());
-        
-        niceString = new NiceString(inputTest3); 
-        System.out.println("is Nice? " + niceString.isNiceString());
-        
-        niceString = new NiceString(inputTest4); 
-        System.out.println("is Nice? " + niceString.isNiceString());
-        
-        niceString = new NiceString(inputTest5); 
-        System.out.println("is Nice? " + niceString.isNiceString());
+         
+        final int inputTestsLen = inputTests.length;
+        NiceString niceString = null;
+
+
+        for(int i=0; i<inputTestsLen; i++) {
+            niceString = new NiceString(inputTests[i]);
+            System.out.println(niceString + " is Nice? " + niceString.isNiceString());
+        }   
         
         final List<String> strings = FileUtils.readLines(Paths.get(inputPartOnePath));
         System.out.println("How many nice strings? " + countNiceStrings(strings));
