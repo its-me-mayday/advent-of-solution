6b657b85a74b2f4cea181d3b48a90ed92676497d
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-21 10:25:25 +0200

    feat(y2015;d6): create grid for coordinates

--- y2015/d6/main.rs
+++ y2015/d6/main.rs
@@ -13,10 +13,23 @@ fn new_coordinate(x: i16, y: i16) -> Coordinate {
 }
 
 fn main() {
+    let mut grid: Vec<Vec<Coordinate>> = Vec::new();
+    
     println!("==== PART ONE, d6: Probably a Fire Hazard ====");
     let mut c = new_coordinate(0,0);
     println!("x: {}, y: {}, lit: {}", c.x, c.y, c.lit);
     c.lit = true;
     println!("x: {}, y: {}, lit: {}", c.x, c.y, c.lit);
+    
+    let mut v = Vec::new();
+    v.push(c);
+    grid.push(v);
+    
+    for vec in &grid {
+        for x in vec { 
+            println!("{}",x.lit);
+        }
+    }
+
     println!("==== END PART ONE: d6: Probably a Fire Hazard ====");
 }
