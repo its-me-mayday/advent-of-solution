c7ec98cec66997c235a8c999419c3ace49b6f18a
Luca Maggio <lucamaggio1992@gmail.com>
2025-10-21 10:39:09 +0200

    feat(y2015;d6): init grid and add print grid fn

--- y2015/d6/main.rs
+++ y2015/d6/main.rs
@@ -12,24 +12,32 @@ fn new_coordinate(x: i16, y: i16) -> Coordinate {
     }
 }
 
-fn main() {
-    let mut grid: Vec<Vec<Coordinate>> = Vec::new();
-    
-    println!("==== PART ONE, d6: Probably a Fire Hazard ====");
-    let mut c = new_coordinate(0,0);
-    println!("x: {}, y: {}, lit: {}", c.x, c.y, c.lit);
-    c.lit = true;
-    println!("x: {}, y: {}, lit: {}", c.x, c.y, c.lit);
-    
-    let mut v = Vec::new();
-    v.push(c);
-    grid.push(v);
-    
-    for vec in &grid {
-        for x in vec { 
-            println!("{}",x.lit);
+fn print_grid(grid: Vec<Vec<Coordinate>>) {
+        for vec in &grid {
+        for c in vec { 
+            println!("x: {}, y: {}, lit: {}", c.x, c.y, c.lit);
         }
     }
+}
 
+fn init_grid() -> Vec<Vec<Coordinate>> {
+    let mut grid: Vec<Vec<Coordinate>> = Vec::new();
+
+    for i in 0..100 {
+        let mut v = Vec::new();
+        
+        for j in 0..100 {
+            v.push(new_coordinate(i,j));
+        }
+        grid.push(v);
+    }
+
+    return grid;
+}
+
+fn main() {
+    println!("==== PART ONE, d6: Probably a Fire Hazard ====");
+    let grid = init_grid();
+    print_grid(grid);
     println!("==== END PART ONE: d6: Probably a Fire Hazard ====");
 }
