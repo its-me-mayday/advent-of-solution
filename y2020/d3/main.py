a358317ef496c1f22a447128cd7c6ed4d8f42bb5
Luca Maggio <lucamaggio1992@gmail.com>
2025-11-06 08:39:47 +0100

    feat(y2020;d3): conclude part two

--- y2020/d3/main.py
+++ y2020/d3/main.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
 
 input_part_one = "./inputs/part1.txt"
 input_part_two = "./inputs/part2.txt"
@@ -21,14 +21,22 @@ count = count_trees(toboggan_map, log)
 print(count)
 print("== PART_ONE: end input ==")
 
-#print("== PART_TWO: tests ==")
-#toboggan_map = read(input_test_one)
-#count = count_trees(toboggan_map, log)
-#print(count)
-#print("== PART_TWO: end tests ==")
+print("== PART_TWO: tests ==")
+toboggan_map = read(input_test_two)
+trees = 1
+for (dx,dy) in slopes:
+    count = count_trees(toboggan_map, log, dx, dy)
+    trees *= count
+    print(count)
+print(trees)
+print("== PART_TWO: end tests ==")
 
-#print("== PART_TWO: input ==")
-#toboggan_map = read(input_test_one)
-#count = count_trees(toboggan_map, log)
-#print(count)
-#print("== PART_TWO: end input ==")
+print("== PART_TWO: input ==")
+toboggan_map = read(input_part_two)
+trees = 1
+for (dx,dy) in slopes:
+    count = count_trees(toboggan_map, log, dx, dy)
+    trees *= count
+    print(count)
+print(trees)
+print("== PART_TWO: end input ==")
--- y2020/d3/utils.py
+++ y2020/d3/utils.py
@@ -1,7 +1,11 @@
 def count_trees(toboggan_map, log=None, dx=3, dy=1):
-    rows = [r.rstrip("\n") for r in toboggan_map]
-    H, W = len(rows), len(rows[0])
-    r = c = trees = 0
+    rows = [r.strip() for r in toboggan_map if r.strip() != ""]
+    H = len(rows)
+    W = len(rows[0])
+
+    r = 0
+    c = 0
+    trees = 0
 
     while True:
         r += dy
@@ -10,5 +14,7 @@ def count_trees(toboggan_map, log=None, dx=3, dy=1):
             break
         if rows[r][c % W] == '#':
             trees += 1
-    if log: log.info(f"trees={trees}")
+
+    if log:
+        log.info(f"trees={trees}")
     return trees
